e4fe20ffe4915f7dc218670e7172b27b521e5136
Stuart Irwin <stuartdi@hotmail.com>
2021-06-26 22:30:01 -0700

    completed simple win32 program

---
+++ fig02_01.cpp
@@ -0,0 +1,10 @@
+//  fig02_01.cpp
+//  text printing program
+#include <iostream>  //  enables program to output data to the screen
+
+//  function main begins program execution
+int main() {
+    std::cout << "Welcome to C++!\n"; //  display message
+
+    return 0; //  indicate that the program ended successfully
+} //  end function main
