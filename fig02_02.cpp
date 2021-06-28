877854f63aab64db3a81ce2c7701542ecce40409
Stuart Irwin <stuartdi@hotmail.com>
2021-06-27 22:06:46 -0700

    concatenate multiple statements

---
+++ fig02_02.cpp
@@ -0,0 +1,9 @@
+// fig02_02.cpp
+// Displaying a line of text with multiple statements
+#include <iostream>
+
+// function main begins program execution
+int main() {
+    std::cout << "Welcome ";
+    std::cout << "to C++!\n";
+} // end function main
