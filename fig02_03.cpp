3e6c978b7a9d0ff0d307d48942e143b7071a2918
Stuart Irwin <stuartdi@hotmail.com>
2021-06-28 19:17:35 -0700

    multiple lines using escape sequence

---
+++ fig02_03.cpp
@@ -0,0 +1,8 @@
+// fig 02_03.cpp
+// displaying multiple lines of text with a single statement
+#include <iostream> // enables output to screen
+
+// function main begins program execution
+int main() {
+    std::cout << "Welcome\nto\n\nC++!\n";
+}  // end function main
