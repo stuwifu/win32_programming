c8e1481e5bc701e311b7b64e9d2f9b5eaa76b392
stuwifu <stuartdi@hotmail.com>
2021-07-02 05:08:28 -0700

    commiting incomplete if statement examle

---
+++ fig02_05.cpp
@@ -0,0 +1,23 @@
+// fig02_05.cpp
+// comparing integers using if statements, relational operators
+// and equality operators
+#include <iostream> // enables program to perform input and output
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// function main
+int main() {
+    int number1{0};
+    int number2{0};
+
+    cout << "Enter two integers to compare: ";
+    cin  >> number1 >> number2; // read two integers from user
+
+    if (number1 == number2) {
+        cout << number1 << " == " << number2 << endl;
+    }
+
+    if (number1 != number2)
+}
